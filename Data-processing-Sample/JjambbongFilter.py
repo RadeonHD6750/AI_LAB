0c6f10f7d4e2c9a0e7c1d226ae00efdeb1d2ae54
jimin <thesuffering@naver.com>
2023-05-11 15:08:16 +0900

    필터류 추가

---
+++ Data-processing-Sample/JjambbongFilter.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from LowpassFilter import *
+from KalmanFilter import *
+
+np.random.seed(0)
+
+def get_volt():
+    """Measure voltage."""
+    v = np.random.normal(0, 2)   # v: measurement noise.
+    volt_true = 14.4             # volt_true: True voltage [V].
+    z_volt_meas = volt_true + v  # z_volt_meas: Measured Voltage [V] (observable).
+    return z_volt_meas
+
+# Input parameters.
+time_end = 10
+dt = 0.2
+x_0 = 12  # 14 for book.
+P_0 = 6
+
+lpf = LowPassFilter(cutoff_freq=5, ts=0.01)
+kalf = KalmanFilter(x_0=x_0, P_0=P_0)
+
+time = np.arange(0, time_end, dt)
+n_samples = len(time)
+
+volt_meas_save = np.zeros(n_samples)
+volt_lpf_save = np.zeros(n_samples)
+volt_esti_save = np.zeros(n_samples)
+
+x_esti = 0
+x_lpf = 0
+
+for i in range(n_samples):
+    z_meas = get_volt()
+
+    x_lpf = lpf.filter(z_meas)
+    x_esti = kalf(z_meas)
+
+    volt_meas_save[i] = z_meas
+    volt_lpf_save[i] = x_lpf
+    volt_esti_save[i] = x_esti
+
+
+plt.plot(time, volt_meas_save, 'r*--', label='Measurements')
+plt.plot(time, volt_lpf_save, 'gv--', label='Lowpass Filter')
+plt.plot(time, volt_esti_save, 'bo-', label='Kalman Filter')
+plt.legend(loc='upper left')
+plt.title('Measurements v.s. Estimation (Kalman Filter)')
+plt.xlabel('Time [sec]')
+plt.ylabel('Voltage [V]')
+plt.show()
+#plt.savefig('png/simple_kalman_filter.png')
--- Data-processing-Sample/KalmanFilter.py
+++ Data-processing-Sample/KalmanFilter.py
@@ -1,71 +1,34 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from LowpassFilter import *
+class KalmanFilter:
+    # Initialization for system model.
+    A = 1
+    H = 1
+    Q = 0
+    R = 4
 
-np.random.seed(0)
+    P = 0
 
-def get_volt():
-    """Measure voltage."""
-    v = np.random.normal(0, 2)   # v: measurement noise.
-    volt_true = 14.4             # volt_true: True voltage [V].
-    z_volt_meas = volt_true + v  # z_volt_meas: Measured Voltage [V] (observable).
-    return z_volt_meas
+    # Initialization for estimation.
+    x_0 = 12  # 14 for book.
+    P_0 = 6
 
+    x_esti = 0
 
-def kalman_filter(z_meas, x_esti, P):
-    """Kalman Filter Algorithm for One Variable."""
-    # (1) Prediction.
-    x_pred = A * x_esti
-    P_pred = A * P * A + Q
+    def __int__(self, x_0, P_0):
+        self.x_esti, P = x_0, P_0
 
-    # (2) Kalman Gain.
-    K = P_pred * H / (H * P_pred * H + R)
+    def filter(self, z_meas):
+        """Kalman Filter Algorithm for One Variable."""
+        # (1) Prediction.
+        x_pred = self.A * self.x_esti
+        P_pred = self.A * self.P * self.A + self.Q
 
-    # (3) Estimation.
-    x_esti = x_pred + K * (z_meas - H * x_pred)
+        # (2) Kalman Gain.
+        K = P_pred * self.H / (self.H * P_pred * self.H + self.R)
 
-    # (4) Error Covariance.
-    P = P_pred - K * H * P_pred
+        # (3) Estimation.
+        x_esti = x_pred + K * (z_meas - self.H * x_pred)
 
-    return x_esti, P
+        # (4) Error Covariance.
+        P = P_pred - K * self.H * P_pred
 
-# Input parameters.
-time_end = 10
-dt = 0.2
-
-# Initialization for system model.
-A = 1
-H = 1
-Q = 0
-R = 4
-# Initialization for estimation.
-x_0 = 12  # 14 for book.
-P_0 = 6
-
-
-time = np.arange(0, time_end, dt)
-n_samples = len(time)
-
-volt_meas_save = np.zeros(n_samples)
-volt_esti_save = np.zeros(n_samples)
-
-x_esti, P = None, None
-for i in range(n_samples):
-    z_meas = get_volt()
-    if i == 0:
-        x_esti, P = x_0, P_0
-    else:
-        x_esti, P = kalman_filter(z_meas, x_esti, P)
-
-    volt_meas_save[i] = z_meas
-    volt_esti_save[i] = x_esti
-
-
-plt.plot(time, volt_meas_save, 'r*--', label='Measurements')
-plt.plot(time, volt_esti_save, 'bo-', label='Kalman Filter')
-plt.legend(loc='upper left')
-plt.title('Measurements v.s. Estimation (Kalman Filter)')
-plt.xlabel('Time [sec]')
-plt.ylabel('Voltage [V]')
-plt.show()
-#plt.savefig('png/simple_kalman_filter.png')
+        return self.x_esti, self.P
